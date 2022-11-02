e2c33c39e24b18a1e2e22b117f3d14b00a0185cb
bakman <berkanakman@gmail.com>
2022-11-02 08:27:26 +0300

    Revert "First"
    This reverts commit 43b2e896a02feb71ae1f721248d6e46c9d953c7c.

--- library/functions.php
+++ library/functions.php
@@ -1,5 +1,32 @@
 <?php
+
     include_once "connection.php";
+
+    function get_all_records(){
+        $Sql = "SELECT * FROM employeeinfo";
+        $result = mysqli_query($conn, $Sql);
+        if (mysqli_num_rows($result) > 0) {
+            echo "<div class='table-responsive'><table id='myTable' class='table table-striped table-bordered'>
+                 <thead><tr><th>EMP ID</th>
+                              <th>First Name</th>
+                              <th>Last Name</th>
+                              <th>Email</th>
+                              <th>Registration Date</th>
+                            </tr></thead><tbody>";
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "<tr><td>" . $row['emp_id']."</td>
+                       <td>" . $row['firstname']."</td>
+                       <td>" . $row['lastname']."</td>
+                       <td>" . $row['email']."</td>
+                       <td>" . $row['reg_date']."</td></tr>";
+            }
+
+            echo "</tbody></table></div>";
+
+        } else {
+            echo "you have no records";
+        }
+    }
 
     function phoneFormater($phone) {
         $phone  = preg_replace("/[^0-9]/", "", $phone);
